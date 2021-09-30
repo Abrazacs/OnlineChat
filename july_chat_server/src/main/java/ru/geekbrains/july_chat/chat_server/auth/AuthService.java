404df82b943f58e17993d6527af9a9203c477a5a
Abrazacs <84628800+Abrazacs@users.noreply.github.com>
2021-10-01 01:11:13 +0300

    ДЗ по JDBC
    Добавлен DataBaseAuthservice

---
+++ july_chat_server/src/main/java/ru/geekbrains/july_chat/chat_server/auth/AuthService.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.july_chat.chat_server.auth;
+
+import java.sql.SQLException;
+
+public interface AuthService {
+    void start();
+    void stop();
+    String getNicknameByLoginAndPassword(String login, String password);
+    String changeNickname(String oldNick, String newNick);
+    void changePassword(String nickname, String oldPassword, String newPassword);
+    void createNewUser(String login, String password, String nickname);
+    void deleteUser(String nickname);
+}
